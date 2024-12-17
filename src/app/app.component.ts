d35c734bc9cae7d0ef6afa42a67ed1866be4b0ac
rohit24-code <rohitsingh17031995@gmail.com>
2024-12-17 22:42:03 +0530

    added a profile component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent,SignupComponent],
+  imports: [LoginComponent, SignupComponent, ProfileComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'hello world';
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  //   template: `<h1>Profile component</h1>`,
+  templateUrl: './profile.component.html',
+})
+export class ProfileComponent {}
